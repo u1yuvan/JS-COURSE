6a4461cb774ff5b968843c49bb99d32a114e91e8
u1yuvan <yuvanraj700@gmail.com>
2022-12-06 19:12:51 +0530

    IF ELSE

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-//BITWISE OPERATORS
-// HUMANCODE ->  MACHINE CODE
+//Conditional statements
+// let isRaining = false;
+// let isCloudy = true;
 
+// if (isRaining  || isCloudy) {
+//     console.log("Don't forgot to take umbrella!");
 
-// w -> 00000100 -> WRITE
-// R -> 00000010 -> READ
-// E -> 00000001 -> EXECUTE
+// }
+// else {
+//     console.log("Sky is normal! Enjoy your day");
+// }
 
+// if hour is between to 12am to 1pm -> Good Morning
+//Else if hour is between 1pm to 6pm -> Good Afternoon
+//Else hour is between 6pm to 12am -> good night
 
+// let hrs = new Date();
+// let hour = hrs.getHours();                                   OR
 
-const readpermission = 4;
-const writepermission = 2;
-const executepermission = 1;
+let hour= new Date().getHours();
+// console.log(hour);
+// console.log(hour.getHours());
 
-let mypermission = 0;
-
-mypermission = mypermission | readpermission |writepermission;
-
-let message = (mypermission & readpermission) ? 'yes': 'no';
-
-console.log(message);
+if (hour >= 0 && hour <= 13)
+   {
+    console.log("Good Morning");
+   }
+   else if( hour >= 13 && hour <= 18){
+    console.log("Good Afternoon");
+   }
+    
+    else
+    {
+        console.log("Good Evening");
+    }
