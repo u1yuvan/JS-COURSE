c9c724d45673b5d10f433cc93f4f160d46c20e37
u1yuvan <yuvanraj700@gmail.com>
2022-12-06 18:07:22 +0530

    BITWISE OPERATORS

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-//Falsy (false)
-// undefined
-// null
-// 0
-// false
-// '' -> ""
-// NaN
-
-// truthy -> Anything that is not falsy is -> Truthy
+//BITWISE OPERATORS
+// HUMANCODE ->  MACHINE CODE
 
 
-let usercolor = undefined;
-let defaultcolor = "blue";
+// w -> 00000100 -> WRITE
+// R -> 00000010 -> READ
+// E -> 00000001 -> EXECUTE
 
-let currentColor = usercolor || defaultcolor
 
-console.log("selected Color: " + currentColor);
+
+const readpermission = 4;
+const writepermission = 2;
+const executepermission = 1;
+
+let mypermission = 0;
+
+mypermission = mypermission | readpermission |writepermission;
+
+let message = (mypermission & readpermission) ? 'yes': 'no';
+
+console.log(message);
