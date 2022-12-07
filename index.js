6bfab78bf5b9c851f09008b7ce720fb049ad786f
u1yuvan <yuvanraj700@gmail.com>
2022-12-07 13:13:14 +0530

    For LOOP

--- index.js
+++ index.js
@@ -1,88 +1,12 @@
-//switch case
-
-// let grade = "B";
-
-// //S
-// //A
-// //B
-// //E
-// //U
-
-
-
-// switch(grade){
-//     case "S":
-//         console.log("Super  Grade");
-//         break;
-//         case "A":
-//             case "B":
-//             console.log("Exelent Grade");
-//             break;
-
-//             case "E":
-//                 console.log("Just Pass!");
-//                 break;
-
-//             case "U":
-//                 console.log("Fail");   
-//                 break;
-                
-//                 default:
-//                     console.log("Unknown Grade");
-
-//         }
-
-
-
-
-// let marks = 99;
-// switch(true){
-//     case marks > 99:
-//         console.log("centum");
-//         break;
-//     case marks > 90:
-//         console.log("Super Grade");
-//         break;
-// case marks > 40:
-//     console.log("Pass");
-//     break;
-    
-//         case marks < 40:
-//             console.log("Failed!");
-//             break;
-            
-//             default: 
-//             console.log("Unknown Grade");
+// for(initialExpression; Condition; Step){
+//     console.log("Number #1");
 // }
 
-//Example: Mobile buying suggestion
-//if given saving amount is > 10k buy Realme mobile
-//Else If given amount is > 20k buy iqoo mobile// Else If given amount is 40k buy iphone 11
-//Else If given amount is  > 500 && < 1k buy nokia 1100
-//Else print "You can't affort mobile phone now!"
+for(let i = 10;i >=1;i--){
 
-let budjet = 300;
-switch(true) {
-    
-    case budjet >=40000:
-        console.log("Iphone 11");
-        break;
-    case budjet >=20000:
-        console.log("Iqoo Z6 pro");
-        break;
-case budjet >=10000:
-    console.log("Realme");
-    break;
-    case budjet >=5000:
-        console.log("Redmi");
-        break;
-    case budjet >=1000:
-        console.log("Nokia 1100");
-        break
-    
-    case budjet <=1000 && budjet >= 500:
-        console.log("Nokia 1100");
-        break;
-        
-        default:
-            console.log("You can't affort mobile phone now!");}
+if(i % 2 == 0){
+    console.log("Even Number #" +i);
+}
+
+  
+}
