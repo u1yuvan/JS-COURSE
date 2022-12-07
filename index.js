4f1fabb00b6f5ca0a32e4cace85c955391047cae
u1yuvan <yuvanraj700@gmail.com>
2022-12-07 13:44:23 +0530

    Do While LOOP

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-let i = 20; // inline variable
+let i = 0; // inline variable
 
-while(i >=1){
+while(i <=20){
 
-if(i % 2 == 0){
-    console.log("Even Number #" +i);
+if(i % 2 !== 0){
+    console.log("Even Number While #" +i);
   } 
-  i--; //condition
+  i++; //condition
+  console.log(i);
 }
+
+do{
+    console.log(i);
+    if(i % 2 !== 0){
+        console.log("Even Number Do While #" +i);
+      } 
+      i++; //condition
+
+} while(i <=20) 
