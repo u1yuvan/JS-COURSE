584f776b33e48f3836894b8d207837f8c6ca034c
u1yuvan <yuvanraj700@gmail.com>
2022-12-10 18:38:28 +0530

    Removing Elements in Array

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-//Adding elements to JavaScript Array
-//PRIMITIVE TYPES
+//Removing Elements in Array
 
+const number = [ 1,2,3,4];
 
-const accessories =[
-  {id:1, item:"headphones", quantity:3},
-  {id:2, item:"wrist watch", quantity:2},
-  {id:3, item:"bluetooth speaker", quantity:1},
-  {id:4, item:"pendrive", quantity:3},
-  {id:5, item:"perfume", quantity:4},
-  {id:6, item:"hair wax", quantity:1},
-  {id:7, item:"trimmer", quantity:1},
-  {id:8, item:"laptop", quantity:2},
-  {id:9, item:"blow dryier", quantity:1},
-]
+//END
+//.PUSH => POP
+// number.pop()
 
-// let result = accessories.find(function(accessories){
-//  return accessories.item ==="laptop"}
-//  )
+//START
+//UNSHIFT => SHIFT 
+// number.shift()
+//MIDDLE
+//SPLICE => SPLICE
+number.splice(0,0)
 
-
-let result = accessories.find( (accessories)=> accessories.item ==="laptop")// Arrow Function
-
- console.log(result);
+console.log(number);
