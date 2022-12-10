597ab0c20e27f4d2c9889ce9e536d3e12cf44170
u1yuvan <yuvanraj700@gmail.com>
2022-12-10 18:13:33 +0530

    Finding Elements in JavaScript (Reference type)

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 //Adding elements to JavaScript Array
-const bikes = ["GT-650","Duke-250","Vitpilen-250"]
+//PRIMITIVE TYPES
 
-//PUSH
-bikes.push("Z900")
-//UNSHIFT
-bikes.unshift("RR-310")
-//SPLICE
-bikes.splice(3,0,"Himalayan", "Swartpillen-250")
-console.log(bikes);
+// const orders = [
+//     {id: 1, item:"Smartphone", Quantity:7},
+//     {id: 2, item:"tab", Quantity:7},
+//     {id: 3, item:"laptops", Quantity:3},]
+
+
+//   let result =  orders.find(function(orders){
+//         return orders.item === "tab"
+//     })
+//     console.log(result);
+
+
+// WORKOUT
+
+const accessories =[
+  {id:1, item:"headphones", quantity:3},
+  {id:2, item:"wrist watch", quantity:2},
+  {id:3, item:"bluetooth speaker", quantity:1},
+  {id:4, item:"pendrive", quantity:3},
+  {id:5, item:"perfume", quantity:4},
+  {id:6, item:"hair wax", quantity:1},
+  {id:7, item:"trimmer", quantity:1},
+  {id:8, item:"laptop", quantity:2},
+  {id:9, item:"blow dryier", quantity:1},
+]
+
+let result = accessories.find(function(accessories){
+ return accessories.item ==="laptop"}
+ )
+
+ console.log(result);
