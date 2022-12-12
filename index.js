e0904b1b54d70fc49b228d010d66df0a83eaeb27
u1yuvan <yuvanraj700@gmail.com>
2022-12-12 13:40:44 +0530

    Joining and Splitting JavaScript ARRAYS

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const dailyRoutine =["Wakeup","Eat","Sleep"];
+//Joining and Splitting JavaScript ARRAYS
 
-// //For-of
-// for(let routine of dailyRoutine){
-//     //Logics
-//     console.log(routine);
-// }
 
-// //For-in
-// for(let routine in dailyRoutine){
-//     console.log(routine,dailyRoutine[routine]);
-// }
 
-//For-Each
-dailyRoutine.forEach(routine=>console.log(routine));//FAT ARROW
+// const dailyRoutine =["Wakeup","Eat","Sleep"];
 
+// let dailyroutines =dailyRoutine.join(", ")        //JOINING
+
+// console.log(dailyroutines);
+
+
+// let fullname = "Yuvan raj";
+// let username = fullname.split(" ");    //SPLITTING
+
+// let firstname = username[0];
+// let lastname =username[1];
+
+// console.log(`My First name is ${firstname} and last name is ${lastname}`);
+
+
+
+// let postTitle = "This is my Post".toUpperCase();         //case sensitive
+// let postslug = postTitle.split(" ");
+// let finalslug = postslug.join('_')
+
+// console.log(finalslug);
+
+
+                                                  //workout
+let mythings = "airpods airdopes blastbox realmebuds2 ".toUpperCase();
+postslug = mythings.split(" ");
+finalslug = postslug.join("---");
+
+
+console.log(finalslug);
