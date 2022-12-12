9902bb9b689fbd858fb3dc15ed211d5b01d8227a
u1yuvan <yuvanraj700@gmail.com>
2022-12-12 20:40:46 +0530

    Mapping a JavScript Array

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-// Allow only adults above 18
-
-// const queue_by_age = [16, 32, 45];
-
-// let adults = queue_by_age.filter(function(value){
-//  return value >=18;
-// });
-
-// console.log("adults",adults);
-
+// Mapping a JavScript Array
 // const numbers = [1,2,3,4,5,6];
 
-// let oddnumbers =  numbers.filter(function(value){
-//     return value % 2 ==1;
+// let final = numbers.map(function(val){
+// return val * 5;
+
 // });
+// console.log(final);
 
-// let evenNumbers = numbers.filter(function(value){
-//     return value % 2 ==0;
-// });
-
-// console.log("Even Numbers: " ,evenNumbers);
-// console.log("Odd Numbers: " ,oddnumbers);
-
-const cart_items = [
-    {id:1,item:"Android Mobiles", cost: 5000},
-    {id:2,item:"Iphone", cost: 35000},
-    {id:3,item:"Android Flagship Mobiles", cost: 50000},
-    {id:4,item:"Microsoft Mobiles", cost: 9000},
-
+const people = [
+    {id:1, first_name:"yuvan", last_name:"raj"},
+    {id:2, first_name:"sri", last_name:"dhar"},
+    {id:3, first_name:"anbu", last_name:"arasan"},
 ];
 
 
 
-let affordable_filter = cart_items.filter((value) => value.cost < 10000);
+let final = people.map(function(val){
+    let fullname =[val.first_name, val.last_name].join(" ");
+    let obj = {id: val.id,fullname: fullname}
+    return obj;
+    
+})
 
-let affordable_find = cart_items.find((value) => value.cost > 10000);
-
-
-
-
-
-
-console.log("affordable_filter: ", affordable_filter);
-console.log("affordable_find: ", affordable_find);
+console.log(final);
