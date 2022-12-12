b0454a7f394e0948aeb947cea4bdf7bd16f1a18b
u1yuvan <yuvanraj700@gmail.com>
2022-12-12 20:21:04 +0530

    Filtering and Find JavaScript Array

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-//Testing Elements in JavaScript Array - Reference types
-const items = [
-    {id:1, name:"Smartphones", isDeliverable:true},
+// Allow only adults above 18
 
-    {id:2, name:"Accessories", isDeliverable:false,
-},
-    {id:3, name:"T.V", isDeliverable:true},
+// const queue_by_age = [16, 32, 45];
+
+// let adults = queue_by_age.filter(function(value){
+//  return value >=18;
+// });
+
+// console.log("adults",adults);
+
+// const numbers = [1,2,3,4,5,6];
+
+// let oddnumbers =  numbers.filter(function(value){
+//     return value % 2 ==1;
+// });
+
+// let evenNumbers = numbers.filter(function(value){
+//     return value % 2 ==0;
+// });
+
+// console.log("Even Numbers: " ,evenNumbers);
+// console.log("Odd Numbers: " ,oddnumbers);
+
+const cart_items = [
+    {id:1,item:"Android Mobiles", cost: 5000},
+    {id:2,item:"Iphone", cost: 35000},
+    {id:3,item:"Android Flagship Mobiles", cost: 50000},
+    {id:4,item:"Microsoft Mobiles", cost: 9000},
+
 ];
 
 
-let AllDeliverables = items.every(function(value){
- return value.isDeliverable == true;
-});
+
+let affordable_filter = cart_items.filter((value) => value.cost < 10000);
+
+let affordable_find = cart_items.find((value) => value.cost > 10000);
 
 
-let notDeliverables = items.some(function(value){
-    return value.isDeliverable == true;
-   });
 
-console.log("AllDeliverables", AllDeliverables);
-console.log("notDeliverables", notDeliverables);
+
+
+
+console.log("affordable_filter: ", affordable_filter);
+console.log("affordable_find: ", affordable_find);
