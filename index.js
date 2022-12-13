5dbfcd27271eeaf53343104d82ec0f7e16ef597d
u1yuvan <yuvanraj700@gmail.com>
2022-12-13 12:59:05 +0530

    JavaScript Arrays : Chaining Method

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-// Mapping a JavScript Array
-// const numbers = [1,2,3,4,5,6];
-
-// let final = numbers.map(function(val){
-// return val * 5;
-
-// });
-// console.log(final);
-
-const people = [
-    {id:1, first_name:"yuvan", last_name:"raj"},
-    {id:2, first_name:"sri", last_name:"dhar"},
-    {id:3, first_name:"anbu", last_name:"arasan"},
+let products = [
+    {id: 1, title:"Android Mobiles",cost:7500},
+    {id: 2, title:"Gaming Computer",cost:90500},
+    {id: 3, title:"Headphones",cost:2500},
 ];
 
-
-
-let final = people.map(function(val){
-    let fullname =[val.first_name, val.last_name].join(" ");
-    let obj = {id: val.id,fullname: fullname}
-    return obj;
-    
-})
-
+                                          // Sympified  the codes from line 20 to 45  in  line 8 to 17 
+let final = products.sort((a,b) =>{ 
+    // return a.cost - b.cost;          //ascending
+ b.cost - a.cost})                      //descending
+ .sort((a,b) => {
+    if (a.title < b.title ) return -1;
+    if (a.title > b.title ) return +1;
+    return 1;})
+.filter((val) => {val.cost <= 8000;})
+.map((val) => {val.title + " - ₹" + val.cost;});
 console.log(final);
+
+                                   
+// //Sort it using Lowest price
+// let sort_by_Lowest_price = products.sort(function(a,b){
+//     // return a.cost - b.cost;          //ascending
+//     return b.cost - a.cost;          //descending
+
+// });
+
+// //Sort it by title ascending 
+// let sort_by_title = sort_by_Lowest_price.sort(function(a,b){
+//     if (a.title < b.title ) return -1;
+//     if (a.title > b.title ) return +1;
+//     return 1;
+// });
+
+// //Filter products less than 8000
+// let sort_by_filter = sort_by_title.filter((val) => {
+//     return val.cost <= 8000;
+// });
+
+// //Map it like This: //Android Phone - 7500
+// let final = sort_by_filter.map((val) => {
+//     return val.title + " - " + val.cost;}
+// );
+
+
+// console.log(final);
