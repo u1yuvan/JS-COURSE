95292a8dd7aab772143d29c852865284169765e5
u1yuvan <yuvanraj700@gmail.com>
2022-12-14 13:01:49 +0530

    Function Arguments

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// Adding Two numbers
+//Function Arguments
+// function sumExpense(){
+//     let total =0;
+
+//     for (let value of arguments){
+// total += value;
+//     }
+//     return total;
+//   }
+
+// let result = sumExpense( 65,250,105,78,15,69,78 );
+
+// console.log("Your Total Expense is: ₹",result);
 
 
-//immediately Invoked Function Expression (IIFE)
-(() => {
-    let no1 = 22356;
-let no2 = 67690;
-
-    let result = no1 + no2;
-    (function displaynumber(){
-        console.log("result: ",result);
-    })();
-//   displaynumber();
-})();
-
-// sumnumbers();
+function Expense(){
+    let total = 0;
+    for(let value of arguments){
+        total += value;
+    }
+    return total;
+}
+let result = Expense(43,35,756,75,2,444645);
+console.log("Your Total Expense is: ", result);
