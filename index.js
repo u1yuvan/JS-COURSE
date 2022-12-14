76508d542282ef03da8b28f4c295e62619b3d7e8
u1yuvan <yuvanraj700@gmail.com>
2022-12-14 19:54:04 +0530

    JavaScript Scopes [Global Scope vs Local Scope]

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-//JS Error Handling (try, throw and catch)
-let student = {
-  firstname: "yuvan",
-  lastname:"raj",
-  get fullname(){
-    return`${student.firstname} ${student.lastname}`;
-  },
+let username = "yuvan";
 
+function greetuser(){
+  let username = "vaishu"
+  let message = "Hello " + username; 
 
-  set fullname(value){
-    if(typeof value != "string") {
-      const error404 =new Error("It is not a String")
-      throw error404;
-    }
-
-    if(value.length <= 3) throw new Error("Name Invalid");
-
-
-   let values =  value.split(" ");
-   this.firstname = values[0];
-   this.lastname = values[1] ?? "";
-  },
-};
-
-
-try{
-  //Defensive Programming
-  student.fullname ="A";
-}catch(ex){
-alert(ex);
+  log(message);
+}
+function log(message){
+  console.log(message);
 }
 
+greetuser();
 
-console.log(student.fullname);
+console.log(username);
