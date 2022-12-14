8e84cd1656a5a5497d5b6fe1909a98c7130d3336
u1yuvan <yuvanraj700@gmail.com>
2022-12-14 17:38:58 +0530

    Rest Operator

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-//Function Arguments
-// function sumExpense(){
-//     let total =0;
+//Rest Operator
+function sumExpense(loan,...expenses){
 
-//     for (let value of arguments){
-// total += value;
-//     }
-//     return total;
-//   }
-
-// let result = sumExpense( 65,250,105,78,15,69,78 );
-
-// console.log("Your Total Expense is: ₹",result);
-
-
-function Expense(){
-    let total = 0;
-    for(let value of arguments){
-        total += value;
-    }
-    return total;
+ let total = expenses.reduce((a, b) => a + b);
+ return total- loan;
 }
-let result = Expense(43,35,756,75,2,444645);
-console.log("Your Total Expense is: ", result);
+
+let result = sumExpense(100, 65,250,105,78,15,69,78 );
+
+console.log("Your Total Expense is: ₹",result);
