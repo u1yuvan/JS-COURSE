950ebc7a0ba474ad650d5372cc0c992198162d10
u1yuvan <yuvanraj700@gmail.com>
2022-12-14 19:28:20 +0530

    JS Error Handling (try, throw and catch)

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+//JS Error Handling (try, throw and catch)
 let student = {
   firstname: "yuvan",
   lastname:"raj",
   get fullname(){
     return`${student.firstname} ${student.lastname}`;
   },
+
+
   set fullname(value){
+    if(typeof value != "string") {
+      const error404 =new Error("It is not a String")
+      throw error404;
+    }
+
+    if(value.length <= 3) throw new Error("Name Invalid");
+
+
    let values =  value.split(" ");
    this.firstname = values[0];
    this.lastname = values[1] ?? "";
   },
 };
-student.fullname ="Arthur"
-// getters => used to get data
-//setters => used to set data
+
+
+try{
+  //Defensive Programming
+  student.fullname ="A";
+}catch(ex){
+alert(ex);
+}
+
 
 console.log(student.fullname);
