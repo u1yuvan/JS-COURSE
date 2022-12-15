72c6351c5374865e65316dd85e4e56c6454c5025
u1yuvan <yuvanraj700@gmail.com>
2022-12-15 13:30:36 +0530

    Difference between Var vs Let keyword

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-let username = "yuvan";
+// Var vs Let
 
-function greetuser(){
-  let username = "vaishu"
-  let message = "Hello " + username; 
-
-  log(message);
-}
-function log(message){
-  console.log(message);
+function displaynumbers(){
+    for ( i = 1; i <= 10; ++i){
+if (true){
+    message = "Hello Everyone!"
 }
 
-greetuser();
+    console.log("My Number is #" + i);
+    }
+    console.log(i);
+    console.log(message);
+}
 
-console.log(username);
+// if keyword not defined, it will be taken as "Var"
+
+//    Var
+//["Var" keyword is accessible in function-scope.]
+//var = function scope   [it will be accessible in this whole function]
+// Var is  merged with window, So it maybe  create some merge conflicts.  
+
+
+//  LET
+// "Let" is only accessible only in block-scope 
+//let = block scope  [ it will not visible out of the block]
+//Let  is a safe, it cannot come out of code block
+
+
+displaynumbers();
